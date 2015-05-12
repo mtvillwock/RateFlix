3cbda467547b522904103a0a69ac7400f3e01905
Matthew Villwock <mtvillwock@gmail.com>
2015-05-12 02:50:06 -0700

    revise controller route and views to get everything working for searching for movies and displaying/creating associated genres and reviews

--- RateFlix/app/controllers/movies_controller.rb
+++ RateFlix/app/controllers/movies_controller.rb
@@ -1,12 +1,15 @@
 class MoviesController < ApplicationController
   include MoviesHelper
   def index
+    @movie = Movie.new
     @movies = Movie.all
     render 'index'
   end
 
   def show
     @movie = Movie.find_by(id: params[:id])
+    @genres = @movie.genres
+    @reviews =  @movie.reviews
     @review = Review.new(user_email: params[:user_email],
                          rating: params[:rating].to_s,
                          date_published: params[:date_published],
@@ -16,14 +19,28 @@ class MoviesController < ApplicationController
   end
 
   def create
+    p "params are: "
     p params
-    movie = Movie.new(title: params[:title],
-                      release_date: params[:release_date],
-                      genre: params[:genre])
-    if movie.save
-      render json: movie
+    title = params[:movie][:title]
+    @movie = Movie.find_by(title: title)
+    p @movie
+    if @movie
+      p "movie already existed"
+      redirect_to "/movies/#{@movie.id}"
+    elsif
+      p "searched movie is being built"
+      response = search(title)
+      p "response hash is: "
+      p response
+      @movie = Movie.create!(title: response[:title],
+                         release_date: response[:release_date],
+                         tmdb_id: response[:tmdb_id])
+      p @movie
+      genres = find_movie_genres(@movie.tmdb_id.to_s)
+      build_movie_genres(movie: @movie, genres: genres)
+      redirect_to "/movies/#{@movie.id}"
     else
-      render json: { error: "movie did not save"}
+      render json: {error: "movie failed to be found"}
     end
   end
 end
--- RateFlix/app/helpers/movies_helper.rb
+++ RateFlix/app/helpers/movies_helper.rb
@@ -19,25 +19,49 @@ module MoviesHelper
     end
   end
 
-  # def genre(genre)
-  #   genre_id = Genre.where(name: genre).first.id
-  #   HTTParty.get("/genre/#{genre_id}/movies?api_key=" + ENV['API_KEY'])
-  # end
+  def find_movie_genres(id)
+    p movie_id = id
+    p response = HTTParty.get(BASE_URI + "/movie/#{movie_id}?api_key=" + ENV['API_KEY'])
+    p movie_genres = response.parsed_response["genres"]
+  end
+
+  def build_movie_genres(args)
+    p args
+    movie = args[:movie]
+    genres = args[:genres]
+    movie_genres = []
+    genres.each do |genre|
+      p genre
+      movie_genres << GenreMovie.create!(movie_id: movie.id, genre_id: Genre.find_by(tmdb_id: genre["id"]).id)
+    end
+    p movie_genres
+  end
 
   def search(title)
-    p title
     p title_query_string = title.downcase.split(" ").join("%20")
     # splits title if multiple words and joins with %20 for spaces
     # http://api.themoviedb.org/3/search/movie?query=die%20hard&api_key=ENV['API_KEY']
-    HTTParty.get("/search/movie" + "?query=#{title_query_string}&api_key=" + ENV['API_KEY'])
+    response = HTTParty.get(BASE_URI + "/search/movie" + "?query=#{title_query_string}&api_key=" + ENV['API_KEY'])
+    movies_array = response.parsed_response["results"]
     # should return collection of movies with that title
     # parse through array of results for movie hash with title == input title
+    @movie_json = {}
+    movies_array.each do |movie|
+      p title
+      p movie["title"]
+      p movie
+      if movie["title"].downcase == title.downcase
+        return @movie_json = { title: movie["title"],
+                         release_date: movie["release_date"],
+                         tmdb_id: movie["id"].to_i }
+      end
+    end
   end
 
   # def release_date(movie)
   #   # movie = Movie.find_by(release_date)
   #   movie_id =
   #   # http://guides.rubyonrails.org/active_record_querying.html#ordering
-  #   HTTParty.get('/movie/#{movie_id}/releases', @options)
+  #   HTTParty.get(BASE_URI + '/movie/#{movie_id}/releases', @options)
   # end
 end
