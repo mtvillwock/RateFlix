c0cc0c2cd42477599965176dc5203927257a53f6
Matthew Villwock <mtvillwock@gmail.com>
2015-05-11 20:17:12 -0700

    add httparty gem for api calls

--- RateFlix/db/schema.rb
+++ RateFlix/db/schema.rb
@@ -11,15 +11,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150511215343) do
+ActiveRecord::Schema.define(version: 20150512031017) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "movies", force: :cascade do |t|
-    t.string "title"
-    t.date   "release_date"
-    t.string "genre"
+    t.string  "title"
+    t.date    "release_date"
+    t.string  "genre"
+    t.integer "tmdb_id"
   end
 
   create_table "reviews", force: :cascade do |t|
