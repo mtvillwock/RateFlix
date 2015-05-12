544395e5ee1c757b43c93cb62c269b2de2f0630a
Matthew Villwock <mtvillwock@gmail.com>
2015-05-12 02:50:48 -0700

    revise review routes and views to get form working and post route operational

--- RateFlix/app/controllers/reviews_controller.rb
+++ RateFlix/app/controllers/reviews_controller.rb
@@ -1,13 +1,20 @@
 class ReviewsController < ApplicationController
+
   def create
     p params
-    review = Review.new(movie_id: params[:movie_id],
-                        user_email: params[:user_email],
-                        date_published: params[:date_published],
-                        rating: params[:rating],
-                        comment: params[:comment])
-    if review.save
-      render json: review
+    p params[:review]
+    p params[:date_published]
+     year = params[:date_published][:year]
+     month = params[:date_published][:month]
+     day = params[:date_published][:day]
+     date_published = "#{year}-#{month}-#{day}"
+    review = Review.create!(movie_id: params[:movie_id],
+                        user_email: params[:review][:user_email],
+                        date_published: date_published,
+                        rating: params[:review][:rating].to_i,
+                        comment: params[:review][:comment])
+    if review
+      redirect_to "/movies/#{review.movie_id}"
     else
       render json: { error: "Review didn't save" }
     end
