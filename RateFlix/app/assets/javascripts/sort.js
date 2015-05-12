dc047f4ed2f0dca564fc7f603b07e14927f99457
Matthew Villwock <mtvillwock@gmail.com>
2015-05-12 05:05:09 -0700

    review html for easier jQuery use, genre sort working

--- RateFlix/app/assets/javascripts/sort.js
+++ RateFlix/app/assets/javascripts/sort.js
@@ -20,17 +20,18 @@ function releaseDateSortListener(){
   $('.container').on('click', 'button.release-date', sortByReleaseDate);
 }
 
-function parseMovies(movies) {
-  // convert HTML to JSON
-  // pass to buildMovie
-}
+// function parseMovies(movies) {
+//   // convert HTML to JSON
+//   // pass to buildMovie
+// }
 
 function buildMovie(data) {
   var movieTemplate = $.trim($('#movie-template').html());
   var $movie = $(movieTemplate);
-  $movie.find('.title').text(data.title);
-  $movie.find('.genre').text(data.genres);
-  $movie.find('.release-date').text(data.release_date);
+  $movie.find('.title a').append(data.title);
+  $movie.find('.title a').attr('href', "/movies/" + data.id)
+  $movie.find('.genre').append(data.genre);
+  $movie.find('.release-date').append(data.release_date);
   return $movie;
 }
 
@@ -40,9 +41,25 @@ function sortByGenre() {
   $('.movie-list').empty();
   // iterate through movies and grab title, genres, and release date
   // build div again
-  for (var i = 0; i < movies.length; i++) {
-    console.log(movies);
-  };
+  $.ajax({
+    url: '/movies/genre',
+    type: 'put',
+  })
+  .done(function(response) {
+    for (var i = 0; i < response.length; i++) {
+      var genre = response[i][0]; // genre
+      var movie = response[i][1]; // object
+      var data = { title: movie.title, genre: genre, release_date: movie.release_date, id: movie.id }
+      // {data: { title: title, genre: genre, release_date: release_date, id: id}}
+      var movieDiv = buildMovie(data);
+      $('.movie-list').append(movieDiv);
+    };
+    console.log("done response is: " + response);
+  }).fail(function(response) {
+    console.log("fail response is: " + response);
+  }).always(function(response) {
+    console.log("always response is: " + response);
+  })
   // debugger;
 };
 
--- RateFlix/app/controllers/movies_controller.rb
+++ RateFlix/app/controllers/movies_controller.rb
@@ -33,14 +33,35 @@ class MoviesController < ApplicationController
       p "response hash is: "
       p response
       @movie = Movie.create!(title: response[:title],
-                         release_date: response[:release_date],
-                         tmdb_id: response[:tmdb_id])
+                             release_date: response[:release_date],
+                             tmdb_id: response[:tmdb_id])
       p @movie
       genres = find_movie_genres(@movie.tmdb_id.to_s)
       build_movie_genres(movie: @movie, genres: genres)
       redirect_to "/movies/#{@movie.id}"
     else
       render json: {error: "movie failed to be found"}
+    end
+  end
+
+  def update
+    p params
+    if params[:id] == "genre"
+      p @movies = Movie.all
+      @genres = []
+      @movies.each do |movie|
+        @genres << movie.genres.first.name
+      end
+      p @genres_and_movies = @genres.zip(@movies)
+      render json: @genres_and_movies
+    elsif params[:id] == "title"
+      @movies_by_title = Movie.order(title: :asc)
+      render json: @movies_by_title
+    elsif params[:id] == "title"
+      @movies_by_release_date = Movie.order(release_date: :asc)
+      render json: @movies_by_release_date
+    else
+      render json: {error: "sort call failed"}
     end
   end
 end
